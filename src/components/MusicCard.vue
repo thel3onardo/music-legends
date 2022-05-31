e9426949b77c32550f2a881ae54ad10643ca82cf
thel3onardo <leonardo.contato03@gmail.com>
2022-05-31 16:57:18 -0300

    feat: develop basic structure of musicCard component

---
+++ src/components/MusicCard.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    const properties = defineProps({
+        imageUrl: {
+            type: String,
+            required: true,
+        }
+    })
+</script>
+
+<template>
+    <div class="w-52 shadow-lg shadow-green-400/40 rounded-xl overflow-hidden transition">
+        <img :src="imageUrl" alt="testing" class="h-full object-cover" />
+    </div>
+</template>
